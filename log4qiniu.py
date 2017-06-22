766c1d200c8c753bf88fb096877e979cfaf6b41a
zhangzheng <zhangzheng@qianbao.com>
2017-06-22 14:42:11 +0800

    增加日志

--- log4qiniu.py
+++ log4qiniu.py
@@ -17,14 +17,19 @@ Options:
 
 import datetime
 import json
+import logging
 import signal
+import os
 from docopt import docopt
 import requests
 import qiniu
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+logging.basicConfig(filename="/var/log/log4qiniu.log", filemode="a+", format="%(asctime)s-%(name)s-%(levelname)s-%(message)s", level=logging.DEBUG)
 
 delay_days = 1
-max_download = 24
+max_download = 48
 verbose = 0
 is_exited = False
 
@@ -40,8 +45,9 @@ def get_date(days):
     date = datetime.datetime.now() + datetime.timedelta(days=-days)
     return date.strftime('%Y-%m-%d')
 
-def write_disk(name, content):
-    with open(name.replace("v2/.", ""), "wb") as code:
+def write_disk(path, name, content):
+    filename = os.path.join(path, name.replace("v2/.", ""))
+    with open(filename, "wb") as code:
         code.write(content)
 
 def rebuild_options(arguments):
@@ -56,6 +62,7 @@ def rebuild_options(arguments):
         verbose += 1
 
 def main():
+    logging.debug("log4qiniu starting")
     signal.signal(signal.SIGINT, onsignal_term)
     arguments = docopt(__doc__, version='1.0.0rc1')
     rebuild_options(arguments)
@@ -65,6 +72,7 @@ def main():
 
     auth = qiniu.Auth(access_key, secret_key)
     cdn = qiniu.CdnManager(auth)
+    logging.info("log4qiniu get data %s at %s" % (domains, log_date))
     result = cdn.get_log_list_data(domains, log_date)
 
     for v in result[0]["data"].values():
@@ -72,11 +80,18 @@ def main():
             url = log["url"]
             if verbose > 0:
                 print "downloading: %s" % (url)
+            logging.info("log4qiniu downloading %s" % (url))
             name = log["name"]
             r = requests.get(url)
-            write_disk(name, r.content)
+            write_disk(BASE_DIR, name, r.content)
             if is_exited:
                 break
+
+    cmd = "gunzip %s/*.gz" % (BASE_DIR)
+    os.system(cmd)
+
+    logging.debug("log4qiniu complate")
+
 
 if __name__ == '__main__':
     main()
